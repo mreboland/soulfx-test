4449270914bc807815aac7dcecd0af47c5d3f86d
Edward Boland <mr.eboland@gmail.com>
2021-06-04 16:00:53 -0400

    comments, additional code

--- src/App.js
+++ src/App.js
@@ -13,59 +13,41 @@ function App() {
       url: "http://api.openweathermap.org/data/2.5/forecast?appid=ef789d33883afc2c00eb596fd81b40cd&q=toronto",
       params: {
         // To ensure metric units
-        units: "metric"
+        units: "metric",
       },
     }).then((res) => {
       // Saving the weather data to a var
       const weatherResults = res.data.list;
-      console.log(res.data.list);
+
       // Store the API results to the weather state
       setWeather(weatherResults);
     });
   }, []);
 
   return (
-    <div>
+    <div className="wrapper">
       {/* Mapping over weather to be able to access data */}
       {weather.map( (temp, key) => {
-        // console.log(temp.main);
-        console.log(temp.weather[0].icon);
         // Returning the data so we can see it on page
         return (
-          <div>
+          <div className="weather">
             <h1>
-              <Moment format="ddd MMMM DD, hh:mm A" unix>
-                {temp.dt}
-              </Moment>
+              {/* Using moment to format date */}
+              <Moment format="ddd MMMM DD, hh:mm A" unix>{temp.dt}</Moment>
             </h1>
-            <p>{temp.main.feels_like} Degrees Celsius</p>
+            {/* Weather api has built in icons, using their feature to use the weather icon value to get the appropriate weather icon */}
+            {/* https://openweathermap.org/weather-conditions */}
             <img
               src={`http://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`}
               alt={temp.weather[0].description}
             />
+            {/* Rounding temp */}
+            <p>{Math.round(temp.main.feels_like)} °C</p>
           </div>
         );
       })}
     </div>
-
   )
 }
 
 export default App;
-
-
-
-// Create an application that gives you the 5-day weather forecast for the City of Toronto, CANADA.
-// This test should take about 2-3 hours.
-
-// Acceptance Criteria
-// Users should be able to view the 5-day weather forecast for the City of Toronto (from
-// OpenWeather API).
-// Users should be able to view the 5-day forecast (today’s and the next 4 days’). Example, if today
-// is July 10th, then we should see the forecast from July 10-14th.
-// The API by default returns a 5-day, 3-hour forecast.
-// Each list item should feature the following details:
-// ● Day (or date)
-// ● Temperature (in degrees celsius)
-// ● Icon or image representing weather conditions
-// ○ Use any icon library you like. An example would be Erik Flower’s weather icons.
